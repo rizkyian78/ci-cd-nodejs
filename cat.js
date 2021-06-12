f931ac1cf5e647c8b59d7dcdd3230dfcb2d8fe03
Rizky Ian Indiarto <rizkyian78@gmail.com>
2021-06-12 13:42:12 +0700

    sadsadasd

--- cat.js
+++ cat.js
@@ -1,22 +1,22 @@
-const mongoose = require("mongoose");
-mongoose
-  .connect("mongodb://localhost:27017/kitten", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
-  })
-  .then(() => console.log("connected database"));
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect("mongodb://localhost:27017/kitten", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: true,
+//   })
+//   .then(() => console.log("connected database"));
 
-const cat = mongoose.model(
-  "Cat",
-  {
-    name: String,
-    age: Number,
-    gender: String,
-  },
-  "Cat"
-);
+// const cat = mongoose.model(
+//   "Cat",
+//   {
+//     name: String,
+//     age: Number,
+//     gender: String,
+//   },
+//   "Cat"
+// );
 
-module.exports = {
-  cat,
-};
+// module.exports = {
+//   cat,
+// };
